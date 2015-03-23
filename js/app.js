72fcf9553f3cbad2f1d0389b64a7f7d5856a4a26
rob.scott <rob.scott@s4rb.com>
2015-03-23 20:05:03 +0000

    Removes post sections from anything small tablets and phones

--- js/app.js
+++ js/app.js
@@ -4,18 +4,14 @@ $(function() {
     var containerStart = $('.post-content').offset().top;
     var containerHeight = $('.post-content').height();
 
-    console.log(containerHeight);
-
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();
 
         if(scrollTop + nav.height() > (containerStart + containerHeight)) {
             nav.addClass('bottom-stick');
-            //nav.css('position', 'relative');
             nav.css('top', containerHeight - nav.height());
         } else {
             nav.removeClass('bottom-stick');
-            //nav.css('position', 'fixed');
             nav.css('top', 0);
         }
 
